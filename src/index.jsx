dfd61ccbd623663088d05d8513bebfd6fc166372
Luiza Senna <eu@luizasenna.com>
2022-04-12 14:26:31 -0300

    todo-app com redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './main/app'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./main/app";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./main/reducers";
 
+import promise from "redux-promise";
+import multi from "redux-multi";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
